3458bcb93396ba8b89116a2f2443990a7f8fa975
Oleg Fedotov <trevor706@gmail.com>
2022-09-17 12:17:23 +0300

    Set button to navigation bar

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -19,12 +19,14 @@ class ViewController: UIViewController {
     }
     
     private func setupView() {
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(red: 252/255, green: 246/255, blue: 232/255, alpha: 1)
         setupNavigationBar()
     }
     
     private func setupNavigationBar() {
-        title = "Tasks list"
+        title = "My Notes"
+        
+        // Set navigation bar basic settings
         navigationController?.navigationBar.prefersLargeTitles = true
         let appearence = UINavigationBarAppearance()
         appearence.configureWithOpaqueBackground()
@@ -33,6 +35,15 @@ class ViewController: UIViewController {
         appearence.backgroundColor = UIColor(red: 235/255, green: 64/255, blue: 52/255, alpha: 1)
         navigationController?.navigationBar.standardAppearance = appearence
         navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        
+        // Set button to navigation bar
+        let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewTask))
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+        rightBarButtonItem.tintColor = .white
+    }
+    
+    @objc private func addNewTask() {
+        
     }
 }
 
