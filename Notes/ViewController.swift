d55bb339aaa1b06d6ac9ed75352c2764a63fee10
Oleg Fedotov <trevor706@gmail.com>
2022-09-17 11:54:38 +0300

    Create Root UINavigationController

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
-
-
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        title = "Tasks list"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let appearence = UINavigationBarAppearance()
+        appearence.configureWithOpaqueBackground()
+        appearence.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearence.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearence.backgroundColor = UIColor(red: 235/255, green: 64/255, blue: 52/255, alpha: 1)
+        navigationController?.navigationBar.standardAppearance = appearence
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+    }
 }
 
